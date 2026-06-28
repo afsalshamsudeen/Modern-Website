c4661275e836d86ae9a37d7e347ac3d73ed93217
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2026-06-28 16:20:38 +0530

    feat(ui): Hero section added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,24 @@
-import { HTMLAttributes } from "react";
+import { ButtonHTMLAttributes } from "react";
 import { cva } from "class-variance-authority";
 
 const classes = cva('border  rounded-full h-12 px-6 font-medium ',{
     variants:{
         variant:{
-            primary:'bg-lime-400 text-neautral-950 border-lime-400',
+            primary:'bg-lime-400 text-neautral-950 border-lime-400 text-neutral-950',
             secondary:'border-white text-white bg-transparent'
+        },
+        size:{
+            sm: "h-10"
         }
     }
 })
 
-export default function Button(props: {variant:"primary" | "secondary"} & HTMLAttributes<HTMLButtonElement>){
-    const {variant, className, ...otherProps} = props;
-    return <button className={classes({variant, className})} {...otherProps}/>
+export default function Button(
+    props: {
+        variant:"primary" | "secondary";
+        size?:"sm";
+
+} & ButtonHTMLAttributes<HTMLButtonElement>){
+    const {variant, className, size,...otherProps} = props;
+    return <button className={classes({variant,size, className})} {...otherProps}/>
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,9 +1,39 @@
+import Button from "@/components/Button";
+import dashboard1 from "@/assets/images/design-example-1.png"
+import dashboard2 from "@/assets/images/design-example-2.png"
+import Image  from 'next/image';
+import Pointer from "@/components/Pointer";
+
 export default function Hero() {
-    return <section>
-        <div className="container">
-            <div>$10M seed fund raised</div>
-            <h1>The AI Workspace Built for Modern Teams</h1>
-            <p>Protect your business with continuous monitoring, instant alerts, and enterprise-grade security powered by AI.</p>
+    return <section className="py-24 overflow-x-clip">
+        <div className="container relative">
+            
+            <div className="absolute -left-32 top-16 hidden lg:block">
+                <Image src={dashboard1} alt="Dashboard image" className="" />
+            </div>
+
+            <div className="absolute -right-64 top-18 hidden lg:block">
+                <Image src={dashboard2} alt="Dashboard Image"/>
+            </div>
+            <div className="absolute left-56 top-96 hidden lg:block">
+                <Pointer name="Anderson"/>
+            </div>
+
+            <div className="absolute right-80 -top-4 hidden lg:block">
+                <Pointer name="Aitana" color="red"/>
+            </div>
+            
+            <div className="flex justify-center">
+
+            <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full font-semibold text-neutral-950">🌟 $10M seed fund raised</div>
+            </div>
+            <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">The AI Workspace Built for Modern Teams</h1>
+            <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">Protect your business with continuous monitoring, instant alerts, and enterprise-grade security powered by AI.</p>
+
+            <form className="flex border border-white/15 rounded-full p-2 mt-8  max-w-lg mx-auto">
+                <input type="email" placeholder="Enter your email" className="bg-transparent px-4 md:flex-1"/>
+                <Button type="submit" variant="primary" className="whitespace-nowrap" size="sm">Sign Up</Button>
+            </form>
         </div>
     </section>;
 }
