742726d88ca445b4dd557b6427a9c78ba6ae9f72
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2026-06-29 06:32:12 +0530

    feat(ui):partner section ui complete

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
             <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">Protect your business with continuous monitoring, instant alerts, and enterprise-grade security powered by AI.</p>
 
             <form className="flex border border-white/15 rounded-full p-2 mt-8  max-w-lg mx-auto">
-                <input type="email" placeholder="Enter your email" className="bg-transparent px-4 md:flex-1"/>
+                <input type="email" placeholder="Enter your email" className="bg-transparent px-4 md:flex-1 w-full"/>
                 <Button type="submit" variant="primary" className="whitespace-nowrap" size="sm">Sign Up</Button>
             </form>
         </div>
