7be324d8f4e5912e1f569936437f952749e5b9e2
afsalshamsudeen <itsmeafsalshamsudeen@gmail.com>
2026-07-01 19:30:16 +0530

    feat(ui):Integration section added

---
+++ src/data/Integratiions.ts
@@ -0,0 +1,5 @@
+export const IntegrationsData = {
+    tag:"Integrations",
+    title:"",
+    description:"Layers seamlessly connects with your favourite tools, making it easy to plug into any workflow and collaborate across platforms."
+}
